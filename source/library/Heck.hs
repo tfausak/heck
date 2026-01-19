37a9f60c8442d708703ceeee4fee62f0d54282e3
Taylor Fausak <taylor@fausak.me>
2026-01-19 13:37:22 -0600

    Add more expectations (#15)

--- source/library/Heck.hs
+++ source/library/Heck.hs
@@ -11,15 +11,20 @@ data Test m n = MkTest
     it :: String -> m () -> n ()
   }
 
-assertEq ::
-  (Stack.HasCallStack, Applicative m, Eq a, Show a) =>
-  Test m n ->
-  -- | expected
-  a ->
-  -- | actual
-  a ->
-  m ()
-assertEq test expected actual =
-  Monad.when (expected /= actual)
-    . assertFailure test
-    $ "expected " <> show expected <> " but got " <> show actual
+assertEq :: (Stack.HasCallStack, Applicative m, Eq a, Show a) => Test m n -> a -> a -> m ()
+assertEq t x y = Monad.unless (x == y) . assertFailure t $ "expected: " <> show x <> " == " <> show y
+
+assertNe :: (Stack.HasCallStack, Applicative m, Eq a, Show a) => Test m n -> a -> a -> m ()
+assertNe t x y = Monad.unless (x /= y) . assertFailure t $ "expected: " <> show x <> " /= " <> show y
+
+assertLt :: (Stack.HasCallStack, Applicative m, Ord a, Show a) => Test m n -> a -> a -> m ()
+assertLt t x y = Monad.unless (x < y) . assertFailure t $ "expected: " <> show x <> " < " <> show y
+
+assertLe :: (Stack.HasCallStack, Applicative m, Ord a, Show a) => Test m n -> a -> a -> m ()
+assertLe t x y = Monad.unless (x <= y) . assertFailure t $ "expected: " <> show x <> " <= " <> show y
+
+assertGt :: (Stack.HasCallStack, Applicative m, Ord a, Show a) => Test m n -> a -> a -> m ()
+assertGt t x y = Monad.unless (x > y) . assertFailure t $ "expected: " <> show x <> " > " <> show y
+
+assertGe :: (Stack.HasCallStack, Applicative m, Ord a, Show a) => Test m n -> a -> a -> m ()
+assertGe t x y = Monad.unless (x >= y) . assertFailure t $ "expected: " <> show x <> " >= " <> show y
